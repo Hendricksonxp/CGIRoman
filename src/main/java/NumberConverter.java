93e39380bd4586dafd3cec96050a2afb003aaa8a
João Loureiro <jploureiro@Joaos-MacBook-Pro.local>
2022-09-09 17:24:50 +0100

    Added LinkedHashMap

--- src/main/java/NumberConverter.java
+++ src/main/java/NumberConverter.java
@@ -1,17 +1,25 @@
+import org.apache.commons.collections.OrderedMap;
+
+import java.util.*;
+
 public class NumberConverter {
 
     String roman;
     int workingNumber;
 
     public String toRoman(int number) {
+        Map<Integer, String> numberToRoman = new LinkedHashMap<>();
+        numberToRoman.put(10, "X");
+        numberToRoman.put(9, "IX");
+        numberToRoman.put(5, "V");
+        numberToRoman.put(4, "IV");
+        numberToRoman.put(1, "I");
+
         workingNumber = number;
         roman = "";
-
-        createRomanString(10, "X");
-        createRomanString(9, "IX");
-        createRomanString(5, "V");
-        createRomanString(4, "IV");
-        createRomanString(1, "I");
+        for( Map.Entry<Integer,String> entry : numberToRoman.entrySet()) {
+            createRomanString(entry.getKey(), entry.getValue());
+        }
 
         return roman;
     }
