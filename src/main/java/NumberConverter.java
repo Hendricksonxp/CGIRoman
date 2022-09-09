a0a3dd6b0648d2c8f62167c75f037688db055498
João Loureiro <jploureiro@Joaos-MacBook-Pro.local>
2022-09-09 17:05:09 +0100

    Added stuff

--- src/main/java/NumberConverter.java
+++ src/main/java/NumberConverter.java
@@ -7,19 +7,16 @@ public class NumberConverter {
         workingNumber = number;
         roman = "";
 
-        // createRomanString(20, theIs(new char[20 % 10], 'X'));
-        while (workingNumber >= 10)
-            createRomanString(10, "X");
+        createRomanString(10, "X");
         createRomanString(5, "V");
         createRomanString(4, "IV");
-        while (workingNumber >= 1)
-            createRomanString(1, "I");
+        createRomanString(1, "I");
 
         return roman;
     }
 
     private void createRomanString(int currentThreshold, String currentRoman) {
-        if (workingNumber >= currentThreshold)
+        while (workingNumber >= currentThreshold)
         {
             roman += currentRoman;
             workingNumber -= currentThreshold;
