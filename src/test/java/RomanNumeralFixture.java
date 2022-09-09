0206c47bbfbaf477d0f3f074ab9c9e4ebc1a8598
Chet Hendrickson <chet@hendricksonxp.com>
2022-09-09 08:43:43 -0400

    worked on fixture

--- src/test/java/RomanNumeralFixture.java
+++ src/test/java/RomanNumeralFixture.java
@@ -2,6 +2,9 @@ public class RomanNumeralFixture {
 
     public int ArabicNumber;
 
+    public void setArabicNumber(int input){
+        ArabicNumber = input;
+    }
     public String RomanNumber(){
         return "?";
     }
