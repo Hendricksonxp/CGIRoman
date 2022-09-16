6b48ee5bdc92faacea0bc71cae7a45cfa08551cb
Chet Hendrickson <chet@hendricksonxp.com>
2022-09-16 10:43:21 -0400

    got 5 to work

--- src/main/java/ArabicConverter.java
+++ src/main/java/ArabicConverter.java
@@ -1,5 +1,13 @@
 public class ArabicConverter {
-    public String convert(int i) {
-        return "I";
+    public String convert(int arabicNumber) {
+        String romanNumber = "";
+        if(arabicNumber >= 5) {
+            romanNumber = "V";
+            arabicNumber = arabicNumber - 5;
+        }
+        for (int i = 0; i < arabicNumber ; i++) {
+            romanNumber = romanNumber + "I";
+        }
+        return romanNumber;
     }
 }
--- src/test/java/RomanNumeralTest.java
+++ src/test/java/RomanNumeralTest.java
@@ -2,22 +2,41 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-
-
 public class RomanNumeralTest {
 
     @Test
     public void hookUp(){
         assertTrue(true);
     }
-
     @Test
     public void oneConvertsToI(){
 
         ArabicConverter converter = new ArabicConverter();
         String result = converter.convert(1);
         assertEquals("I", result);
-
-
+    }
+    @Test
+    public void twoConvertsToII(){
+        ArabicConverter converter = new ArabicConverter();
+        String result = converter.convert(2);
+        assertEquals("II", result);
+    }
+    @Test
+    public void threeConvertsToIII(){
+        ArabicConverter converter = new ArabicConverter();
+        String result = converter.convert(3);
+        assertEquals("III", result);
+    }
+    @Test
+    public void fiveConvertsToV(){
+        ArabicConverter converter = new ArabicConverter();
+        String result = converter.convert(5);
+        assertEquals("V", result);
+    }
+    @Test
+    public void sixConvertsToVI(){
+        ArabicConverter converter = new ArabicConverter();
+        String result = converter.convert(6);
+        assertEquals("VI", result);
     }
 }
