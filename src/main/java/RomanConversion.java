156f18c980a85bd1191d7fec84aae24ef23ec01e
Chet Hendrickson <chet@hendricksonxp.com>
2022-11-09 12:13:18 -0500

    added 900, 400, 40

--- src/main/java/RomanConversion.java
+++ src/main/java/RomanConversion.java
@@ -6,9 +6,13 @@ public class RomanConversion {
 
         LinkedHashMap<Integer, String> conversionTable = new LinkedHashMap<Integer, String>();
         conversionTable.put(1000,"M");
+        conversionTable.put(900,"CM");
         conversionTable.put(500,"D");
+        conversionTable.put(400,"CD");
         conversionTable.put(100,"C");
+        conversionTable.put(90,"XC");
         conversionTable.put(50,"L");
+        conversionTable.put(40,"XL");
         conversionTable.put(10,"X");
         conversionTable.put(9,"IX");
         conversionTable.put(5, "V");
